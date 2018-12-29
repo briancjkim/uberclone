018f0b729694410766dfa154a6e2dc4d6c9d6ac1
brian <briancjkim91@gmail.com>
2018-12-29 20:00:14 +1100

    typechecking grpahql argument

---
+++ src/api/Hello/sayBye/sayBye.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    sayBye: () => "bye how are uo"
+  }
+}
+
+export default resolvers;
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+import * as logger from 'morgan';
 import { GraphQLServer } from 'graphql-yoga';
-import cors from 'cors';
-import helmet from 'helmet';
-import logger from 'morgan';
+import schema from './schema';
 
 class App {
   public app: GraphQLServer;
   constructor() {
     this.app = new GraphQLServer({
-
+      schema
     });
+    this.middlewares();
   }
   private middlewares = (): void => {
     this.app.express.use(cors());
     this.app.express.use(logger("dev"));
+    this.app.express.use(helmet());
   }
 }
+
+export default new App().app;
+
